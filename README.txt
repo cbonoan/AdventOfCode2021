f99ea8943a43d4f46a5385d557cc0aa3b44ead30
Charles Bonoan <charlesbonoan@gmail.com>
2021-12-01 14:39:02 -0800

    Completed day 1

---
+++ README.txt
@@ -0,0 +1,5 @@
+# Advent Of Code 2021 
+Advent of Code is an Advent calendar that contains different programming puzzles. I thought this challenge would be a great way for me to not only 
+make sure I can still code and freshen up my problem solving skills, but also combine two of my favorite things into one - Christmas and coding! 
+
+This repo will contain the source code (coded in Python) of each puzzle. 
---
+++ day1/day1.py
@@ -0,0 +1,39 @@
+import sys
+
+def getMeasurementIncreases(depths):
+    ## PART 1 ##
+    # prevDepth = int(depths[0])
+    # res = 0
+    # for i in range(1, len(depths)):
+    #     curDepth = int(depths[i])
+    #     if(curDepth > prevDepth):
+    #         res += 1
+    #     prevDepth = curDepth
+
+    # return res
+
+    ## PART 2 - THREE MESAUREMENT SLIDING WINDOW ##
+    prevSum = sum([int(depths[i]) for i in range(3)])
+    res = 0
+    for i in range(1, len(depths)):
+        curSum = 0
+        for j in range(i, i+3):
+            try:
+                curSum += int(depths[j])
+            except IndexError as e:
+                return res
+        if(curSum > prevSum):
+            res += 1
+        prevSum = curSum
+    
+    return res
+
+if __name__ == '__main__':
+    filePath = f"../{sys.argv[1]}"
+
+    with open(filePath) as f:
+        contents = f.readlines()
+
+    # Day 1
+    increases = getMeasurementIncreases(contents)
+    print(increases)
